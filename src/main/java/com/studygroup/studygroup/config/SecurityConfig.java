54b04725d5431809354bf6870f1b9e92ec1e51dc
Jealyang <jaelyangchoi@gmail.com>
2025-02-11 20:16:01 +0900

    feat: "회원 가입 컨트롤러"

---
+++ src/main/java/com/studygroup/studygroup/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.studygroup.studygroup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/sign-up", "/login", "/check-email", "/check-email-token",
+                                "/email-login", "check-email-login", "login-link").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
