0684da5188887f2484f6b7dfdf9056bd7a5946ac
Jealyang <jaelyangchoi@gmail.com>
2025-02-11 22:06:59 +0900

    feat: "회원 가입 폼 서브밋 처리"

--- src/main/java/com/studygroup/studygroup/account/AccountController.java
+++ src/main/java/com/studygroup/studygroup/account/AccountController.java
@@ -1,7 +1,10 @@
 package com.studygroup.studygroup.account;
 
+import com.studygroup.studygroup.domain.Account;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
 
     //바인딩 설정
     @InitBinder("signUpForm") //타입의 케멀케이스로 적용
@@ -35,6 +40,26 @@ public class AccountController {
         if (errors.hasErrors()) {
             return "account/sign-up";
         }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) //TODO encoding 해야함
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+
+        Account newAccount = accountRepository.save(account);
+
+        // TODO 이메일
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디올래, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" +newAccount.getEmailCheckToken()+"&email="+newAccount.getEmail());
+        javaMailSender.send(mailMessage);
 
         return "redirect:/";
     }
