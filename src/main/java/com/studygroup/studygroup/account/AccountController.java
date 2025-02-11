2c5f394e1ee7729d63e4c43a56bd457f2919aca6
Jealyang <jaelyangchoi@gmail.com>
2025-02-11 21:46:40 +0900

    feat: "회원 가입 폼 서브밋 검증"

--- src/main/java/com/studygroup/studygroup/account/AccountController.java
+++ src/main/java/com/studygroup/studygroup/account/AccountController.java
@@ -1,16 +1,41 @@
 package com.studygroup.studygroup.account;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    //바인딩 설정
+    @InitBinder("signUpForm") //타입의 케멀케이스로 적용
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
 //        model.addAttribute("signUpForm", new SignUpForm());
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        return "redirect:/";
     }
 }
