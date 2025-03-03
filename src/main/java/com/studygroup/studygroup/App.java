6fea957bcb0b24a31cdbc38661d053be31bda798
Jealyang <jaelyangchoi@gmail.com>
2025-03-03 20:29:19 +0900

    feat: "회원 가입 메인 네비게이션 메뉴"

--- src/main/java/com/studygroup/studygroup/App.java
+++ src/main/java/com/studygroup/studygroup/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudygroupApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudygroupApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
