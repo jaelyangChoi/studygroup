2aaeec20a978b8f1c146c1868ef0db8d61709c46
Jealyang <jaelyangchoi@gmail.com>
2025-03-09 19:42:58 +0900

    test: "프로필 수정 테스트 - @WithUserDetails"

---
+++ src/test/java/com/studygroup/studygroup/settings/SettingsControllerTest.java
@@ -0,0 +1,72 @@
+package com.studygroup.studygroup.settings;
+
+import com.studygroup.studygroup.account.AccountRepository;
+import com.studygroup.studygroup.account.AccountService;
+import com.studygroup.studygroup.account.SignUpForm;
+import com.studygroup.studygroup.domain.Account;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+class SettingsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname("cjl0701");
+        signUpForm.setEmail("cjl0701@email.com");
+        signUpForm.setPassword("12345678");
+        accountService.processNewAccount(signUpForm);
+    }
+
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll();
+    }
+
+    @WithUserDetails(value = "cjl0701", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("프로필 수정하기 - 입력값 정상")
+    @Test
+    void updateProfile() throws Exception {
+        String bio = "짧은 소개를 수정하는 경우";
+        mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
+                        .param("bio", bio)
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingsController.SETTINGS_PROFILE_URL))
+                .andExpect(flash().attributeExists("message"))
+        ;
+
+
+        Account account = accountRepository.findByNickname("cjl0701");
+        assertEquals(bio, account.getBio());
+
+    }
+
+
+}
